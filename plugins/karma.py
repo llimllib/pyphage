ad8df7bd5c1543b8a495d2f596ff6aa3ab7a0302
bill <bill.mill@gmail.com>
2011-04-09 10:03:26 -0400

    seems that the karma plugin works... rockin

--- plugins/karma.py
+++ plugins/karma.py
@@ -1,11 +1,24 @@
 """/karma: who in the python room has the most karma?"""
-import cPickle
+import re
 
 from chatbot import send, query
 
 query("""
     CREATE TABLE IF NOT EXISTS karma (user_id INTEGER, user_name STRING, karma INTEGER);
 """)
+
+def on_unstar(message):
+    unstarred = message["star"]["message"]["user"]
+    unstarrer = message["star"]["user"]
+
+    #starring yourself? lame.
+    if unstarred['id'] == unstarrer['id']: return
+
+    if query("SELECT * FROM karma WHERE user_id=?", unstarred['id']):
+        query("UPDATE karma SET karma = karma - 10 WHERE user_id=?", unstarred['id'])
+    else:
+        query("""INSERT INTO karma (user_id, user_name, karma)
+                 VALUES (?, ?, ?)""", unstarred['id'], unstarred['username'], 10)
 
 def on_star(message):
     starred = message["star"]["message"]["user"]
@@ -14,7 +27,7 @@ def on_star(message):
     #starring yourself? lame.
     if starred['id'] == starrer['id']: return
      
-    if query("SELECT * FROM karma WHERE user_id=?", starred['id']).fetchall():
+    if query("SELECT * FROM karma WHERE user_id=?", starred['id']):
         query("UPDATE karma SET karma = karma + 10 WHERE user_id=?", starred['id'])
     else:
         query("""INSERT INTO karma (user_id, user_name, karma)
@@ -26,6 +39,6 @@ def on_message(message):
 
     msg = ""
     for user, karma in query("SELECT user_name, karma FROM karma ORDER BY karma DESC LIMIT 5"):
-        msg += "%s: %s" % (user, karma)
+        msg += "%s: %s\n" % (user, karma)
 
     send(message['topic']['id'], msg)
