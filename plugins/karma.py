0afac9a6dc55e39eb1c9a6b7bed96732c77f478d
bill <bill.mill@gmail.com>
2011-04-09 10:18:32 -0400

    rearranging karma

--- plugins/karma.py
+++ plugins/karma.py
@@ -1,4 +1,4 @@
-"""/karma: who in the python room has the most karma?"""
+"""/karma: who in the python group has the most karma?"""
 import re
 
 from chatbot import send, query
@@ -6,19 +6,6 @@ from chatbot import send, query
 query("""
     CREATE TABLE IF NOT EXISTS karma (user_id INTEGER, user_name STRING, karma INTEGER);
 """)
-
-def on_unstar(message):
-    unstarred = message["star"]["message"]["user"]
-    unstarrer = message["star"]["user"]
-
-    #starring yourself? lame.
-    if unstarred['id'] == unstarrer['id']: return
-
-    if query("SELECT * FROM karma WHERE user_id=?", unstarred['id']):
-        query("UPDATE karma SET karma = karma - 10 WHERE user_id=?", unstarred['id'])
-    else:
-        query("""INSERT INTO karma (user_id, user_name, karma)
-                 VALUES (?, ?, ?)""", unstarred['id'], unstarred['username'], 10)
 
 def on_star(message):
     starred = message["star"]["message"]["user"]
@@ -32,6 +19,18 @@ def on_star(message):
     else:
         query("""INSERT INTO karma (user_id, user_name, karma)
                  VALUES (?, ?, ?)""", starred['id'], starred['username'], 10)
+
+def on_unstar(message):
+    unstarred = message["star"]["message"]["user"]
+    unstarrer = message["star"]["user"]
+
+    if unstarred['id'] == unstarrer['id']: return
+
+    if query("SELECT * FROM karma WHERE user_id=?", unstarred['id']):
+        query("UPDATE karma SET karma = karma - 10 WHERE user_id=?", unstarred['id'])
+    else:
+        query("""INSERT INTO karma (user_id, user_name, karma)
+                 VALUES (?, ?, ?)""", unstarred['id'], unstarred['username'], 10)
 
 def on_message(message):
     r = re.search(r"\/karma", message['message'])
