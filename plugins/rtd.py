26510cd8175ff902a6d33ee2b459e6cfffec74bc
bill <bill.mill@gmail.com>
2011-04-09 09:37:20 -0400

    adding the semi-working rtd plugin

---
+++ plugins/rtd.py
@@ -0,0 +1,16 @@
+"""/rtd <module> <function>: return a link to readthedocs for <module> and <function>"""
+import re
+from urllib import quote
+
+from chatbot import send
+
+def on_message(message):
+    r = re.search(r"\/rtd ([\w._-]+) ([\w._-]+)?", message[u'message'])
+    if not r: return
+
+    project = r.group(1)
+    term = r.group(2) or ""
+
+    link = "http://%s.rtfd.org/%s" % (quote(project), quote(term))
+
+    send(message['topic']['id'], link)
